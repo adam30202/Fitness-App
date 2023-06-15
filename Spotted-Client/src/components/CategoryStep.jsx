c43955fe5767e2348be074396f6c73772759376c
adam30202 <128291321+adam30202@users.noreply.github.com>
2023-06-15 22:13:29 +1000

    Likes plural

--- Spotted-Client/src/components/CategoryStep.jsx
+++ Spotted-Client/src/components/CategoryStep.jsx
@@ -9,7 +9,7 @@ return (
         </div>
         <input 
             type="text" 
-            class="block w-full p-2  border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="block w-full p-2  border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Write a category"
             maxLength={ 27 }
             value={ formData.category }
