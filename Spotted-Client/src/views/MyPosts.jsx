c4ede82d48e3ef1190c29399641f4d2b6d289732
adam30202 <128291321+adam30202@users.noreply.github.com>
2023-06-15 19:37:14 +1000

    Removed needless code from server

--- Spotted-Client/src/views/MyPosts.jsx
+++ Spotted-Client/src/views/MyPosts.jsx
@@ -8,6 +8,7 @@ const MyPosts = () => {
 
     const user = useContext(UserContext);
     const [ posts, setPosts ] = useState([]);
+    const [ requestComplete, setRequestComplete] = useState(false);
 
     useEffect(() => {
         if (!user) return
@@ -19,6 +20,7 @@ const MyPosts = () => {
                 // Sorts posts in from newest to oldest
                 const sortedPosts = response.data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
                 setPosts(sortedPosts);
+                setRequestComplete(true)
             })
             .catch((error) => {
                 console.error(error);
@@ -41,7 +43,14 @@ const MyPosts = () => {
     return (
         <div className="container">
             <h1 className="view-title dark:text-white">My Posts</h1>
-            { posts && (posts.map((post) => <DisplayedPost post={ post } deletePost={ deletePost }key={ post._id }/> ))}
+            
+            {requestComplete && (
+                posts.length !== 0 ? (
+                    posts.map((post) => <DisplayedPost post={ post } deletePost={ deletePost }key={ post._id }/> )
+             ) : (
+                <h3 className="nothing-to-show dark:text-white">No posts to show</h3>
+             )
+            )}
         </div>
     );
 }
